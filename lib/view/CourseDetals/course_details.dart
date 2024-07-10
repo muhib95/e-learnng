09056f61036df3e79d20765cd634bed708b64e29
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2024-07-10 17:02:43 +0600

    details

--- lib/view/CourseDetals/course_details.dart
+++ lib/view/CourseDetals/course_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:second/utils/color.dart';
 import 'package:second/wigets/custom_appbar.dart';
+import 'package:second/wigets/custom_button.dart';
 import 'package:video_player/video_player.dart';
 
 class CourseDetails extends StatefulWidget {
@@ -37,84 +38,124 @@ class _CourseDetailsState extends State<CourseDetails> {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     print(widget.courseDetails);
-    return Scaffold(
-      appBar:
-          customAppBar(context: context, title: widget.courseDetails['name']),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: size.height * .25,
-              decoration: BoxDecoration(
-                color: AppColor.backgroundColor,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(.2),
-                    blurRadius: 8,
-                    spreadRadius: .5,
-                  )
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar:
+            customAppBar(context: context, title: widget.courseDetails['name']),
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: size.height * .25,
+                decoration: BoxDecoration(
+                  color: AppColor.backgroundColor,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(.2),
+                      blurRadius: 8,
+                      spreadRadius: .5,
+                    )
+                  ],
+                ),
+                child: _controller.value.isInitialized
+                    ? AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: VideoPlayer(_controller),
+                      )
+                    : Center(child: Text('No video')),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Text(widget.courseDetails['name']),
+              Row(
+                children: [
+                  Text('Ceated By'),
+                  SizedBox(
+                    width: 4.0,
+                  ),
+                  Text(
+                    'Muhibbul Hasan',
+                    style: TextStyle(
+                        color: AppColor.primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ],
               ),
-              child: _controller.value.isInitialized
-                  ? AspectRatio(
-                      aspectRatio: _controller.value.aspectRatio,
-                      child: VideoPlayer(_controller),
-                    )
-                  : Center(child: Text('No video')),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            Text(widget.courseDetails['name']),
-            Row(
-              children: [
-                Text('Ceated By'),
-                SizedBox(
-                  width: 4.0,
-                ),
-                Text(
-                  'Muhibbul Hasan',
-                  style: TextStyle(
-                      color: AppColor.primaryColor,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 4.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
+              SizedBox(
+                width: 4.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.star_border_outlined),
+                      Text('4.8'),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Icon(Icons.watch_later_outlined),
+                      Text('72 hours')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '\$40',
+                        style: TextStyle(
+                            color: AppColor.primaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 4.0,
+              ),
+              TabBar(tabs: [
+                Tab(text: 'Playlist (22)'),
+                Tab(text: 'Description'),
+              ]),
+              SizedBox(
+                width: 4.0,
+              ),
+              Expanded(
+                child: TabBarView(
+                    children: [
+                  Center(child: Text('Playlist')),
+                  Center(child: Text('Description')),
+                ]),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                color: Colors.white,
+                child: Row(
                   children: [
-                    Icon(Icons.star_border_outlined),
-                    Text('4.8'),
-                    SizedBox(
-                      width: 5.0,
-                    ),
-                    Icon(Icons.watch_later_outlined),
-                    Text('72 hours')
+                    CustomButton(buttonText: 'Enroll Now')
                   ],
                 ),
-                Row(
-                  children: [
-                    Text(
-                      '\$40',
-                      style: TextStyle(
-                          color: AppColor.primaryColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    )
-                  ],
-                ),
-              ],
+              ),
             ),
           ],
         ),
+
       ),
     );
   }
